c43ea9260db6cc7bdc07301786da5c924b9e3a8b
rabbit21xx13 <rabbit21xx13@users.noreply.github.com>
2020-12-21 17:03:03 +0500

    Task 01

--- task_01.py
+++ task_01.py
@@ -1,13 +1,14 @@
 import io
+# вариант 3
 
-all_data = []
+all_data = []  # чтение файла
 with io.open('students.txt', encoding='utf-8') as students:
     for line in students:
         all_data.append(line.replace('\n', '').split(' - '))
 
-all_data = sorted(all_data, key=lambda x: x[0])
+all_data = sorted(all_data, key=lambda x: x[0])  # сортировка
 
-sorted_students = open('sorted_students.txt', 'w', encoding='utf-8')
+sorted_students = open('sorted_students.txt', 'w', encoding='utf-8')  # запись в файл
 for student in all_data:
     sorted_students.write(student[0] + ' - ' + student[1] + '\n')
 
